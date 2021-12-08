b95764bc7b0f9270a5cfa6e51e76d92b10985579
PJ <olenderpj1@gmail.com>
2021-12-07 17:40:30 -0700

    second test

--- csim.c
+++ csim.c
@@ -1,209 +1,236 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "cachelab.h"
 #include <getopt.h>
-#include <math.h>
-#include <strings.h>
+#include <unistd.h>
+#include "cachelab.h"
 
-typedef unsigned long long int address;
-typedef int bool;
+const int m = sizeof(long) * 8;
+int hitCount = 0, missCount = 0, evictCount = 0;
+int vFlag = 0, hFlag = 0;
+int s, b, E, t;
+char *trace;
 
-#define TRUE 1
-#define FALSE 0
 
-struct Parameter {
-    int S, s, E, b, B;
-};
-struct cacheResult {
-    int e, m, h;
-};
-struct Line {
-    address tag;
-    bool valid;
-    int used;
-    char* block;
-};
-struct Set {
-    struct Line* lines;
-};
-struct Cache {
-    struct Set* sets;
+struct Line
+{
+    int valid ;
+    unsigned tag ;
+    struct Line *next;
 };
 
-void printUsage();
-struct Cache cacheInit(int lineNum, long long blockSize, long long setNum, struct Cache C);
-int indexRet(int* used, int lineNum, struct Set tempSet, bool empty);
-struct cacheResult runCache(struct Cache cache, struct cacheResult result, address addr, int lineNum, int temp, int b);
+struct Set
+{
+    int E;
+    struct Line *headLine;
+    struct Set *next;
+};
 
-int main(int argc, char* argv[]) {
-    struct cacheResult result, resultS, resultM, resultL, resultBuffer;
-    result.e = 0;
-    result.h = 0;
-    result.m = 0;
-    struct Cache cache;
-    address addr;
-    struct Parameter parameter;
-    FILE *traceIn;
-    char buffer, type;
-    int verbosity = 0;
+struct Cache
+{
+    int S;
+    struct Set *headSet;
+};
+
+// return the number of valid lines in a set
+int set_size(struct Set *set)
+{
+    struct Line *curr = set->headLine;
     int size = 0;
-    int temp = 0;
-    long long setNum = 0;
-    long long blockSize = 0;
-
-    while ((buffer = getopt(argc, argv, "s:E:b:t:vh")) != -1) {
-        switch(buffer) {
-            case 's': temp = atoi(optarg); break;
-            case 'E': parameter.E = atoi(optarg); break;
-            case 'b': parameter.b = atoi(optarg); break;
-            case 't': traceIn = fopen(optarg, "r"); break;
-            case 'v': verbosity = TRUE; break;
-            default:
-            case 'h': printUsage(); exit(1);
-        }
+    while (curr != NULL){
+        ++size;
+        curr = curr->next;
     }
-    if (temp == 0 || parameter.E == 0 || parameter.b == 0 || traceIn == 0) {
-        printf("ERROR - Incorrect parameters entered\n");
-        printUsage();
-        exit(1);
+    return size;
+}
+
+// initialize set with capacity E and no valid lines
+void init_set(struct Set *set, int E){
+    set->E = E;
+    set->headLine = NULL;
+    set->next = NULL;
+}
+
+// initialize cache with 2^s empty sets
+void init_cache(struct Cache *cache, int s, int E) {
+    cache->S = (1 << s);
+    struct Set *thisset = (struct Set*)malloc(sizeof(struct Set));
+    init_set(thisset, E);
+    cache->headSet= thisset;
+    int i = 1;
+    for (i ; i < cache->S; ++i){
+        struct Set *nextset = (struct Set*)malloc(sizeof(struct Set));
+        init_set(nextset, E);
+        thisset->next = nextset;
+        thisset = nextset;
     }
+}
 
-    setNum = pow(2, (float)temp);
-    temp += parameter.b;
-    cache = cacheInit(parameter.E, blockSize, setNum, cache);
-    if (traceIn == 0) {
-        printf("ERROR - Unable to load trace\n");
-        exit(1);
+// delete the last line of a set
+void delete_last_line(struct Set *set){
+    struct Line *curr = set->headLine;
+    struct Line *prev = NULL;
+    struct Line *prev2 = NULL;
+    while (curr != NULL){
+        prev2 = prev;
+        prev = curr;
+        curr = curr->next;
     }
-    if (traceIn != NULL) {
-        while (fscanf(traceIn, " %c %llx,%d", &type, &addr, &size) == 3) {
-            if (type != 'I') {
-                if (verbosity == TRUE) {
-                    resultBuffer = runCache(cache, resultBuffer, addr, parameter.E, temp, parameter.b);
-                    switch(type) {
-                        case 'M': resultM = resultBuffer; break;
-                        case 'S': resultS = resultBuffer; break;
-                        case 'L': resultL = resultBuffer; break;
-                    }
-                    result = resultBuffer;
-                }
-                else {
-                    result = runCache(cache, result, addr, parameter.E, temp, parameter.b);
-                }
-            }
-        }
+    if (prev2 != NULL){
+        prev2->next = NULL;
+    } else {
+        set->headLine = NULL;
     }
+    if (prev != NULL) free(prev);
+}
 
-    if (verbosity == TRUE) {
-        printf("Results for M: ");
-        printSummary(resultM.h, resultM.m, resultM.e);
-        printf("Results for S: ");
-        printSummary(resultS.h, resultS.m, resultS.e);
-        printf("Results for L: ");
-        printSummary(resultL.h, resultL.m, resultL.e);
+// add a (valid) line to the head of a set, evict when exceeds capacity
+void add_line_to_head(struct Set *set, struct Line *line){
+    if (set_size(set) == set->E){
+        delete_last_line(set);
+        evictCount++;
+        if (vFlag) printf("eviction ");
     }
+    line->next = set->headLine;
+    set->headLine = line;
+}
 
-    printSummary(result.h, result.m, result.e);
-    fclose(traceIn);
-    return 0;
-} // main
-
-
-void printUsage() {
-    printf("Usage: ./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
-} // printUsage
-
-
-struct Cache cacheInit(int lineNum, long long blockSize, long long setNum, struct Cache C) {
-    struct Set tempSet;
-    struct Line tempLine;
-    int i, j;
-    C.sets = (struct Set*)malloc(sizeof(struct Set)*setNum);
-    for (j = 0; j < setNum; j++) {
-        tempSet.lines = (struct Line*)malloc(sizeof(struct Line)*lineNum);
-        C.sets[j] = tempSet;
-        for (i = 0; i < lineNum; i++) {
-            tempLine.used = 0;
-            tempLine.tag = 0;
-            tempLine.valid = FALSE;
-            tempSet.lines[i] = tempLine;
-        }
+// move the matched line to head of the set
+void move_line_to_head(struct Set *set, struct Line *line, struct Line *prev){
+    if (prev != NULL){
+        prev->next = line->next;
+        line->next = set->headLine;
+        set->headLine = line;
     }
-    return C;
-} // cacheInit
+}
 
+// the procedure of fetching an address in the cache
+void access_cache(struct Cache *cache, unsigned address){
+    int tag_bits = address >> (s+b);
+    int set_bits = (address << t) >> (t+b);
 
-int indexRet(int* used, int lineNum, struct Set tempSet, bool empty) {
+    // move to target_set according to set_bits
+    struct Set *target_set = cache->headSet;
     int i;
-    int mostFreq = tempSet.lines[0].used;
-    int leastFreq = mostFreq;
-    int sizeBuffer, j = 0;
-    for (i = 1; i < lineNum; i++) {
-        sizeBuffer = tempSet.lines[i].used;
-        if (mostFreq < sizeBuffer) {
-            mostFreq = sizeBuffer;
+    for (i = 0; i < set_bits; ++i){
+        target_set = target_set->next;
+    }
+
+    struct Line *ln = target_set->headLine;
+    struct Line *prev = NULL;
+    while (ln != NULL){
+        // if there is a  hit
+        if (ln->valid && (ln->tag == tag_bits)){
+            hitCount++;
+            if (vFlag) printf("hit ");
+            move_line_to_head(target_set, ln, prev);
+            return ;
         }
-        if (leastFreq > sizeBuffer) {
-            j = i;
-            leastFreq = sizeBuffer;
+        prev = ln;
+        ln = ln->next;
+    }
+
+    // if there is no match then miss and add a new line
+    missCount++;
+    if (vFlag) printf("miss ");
+    struct Line *newln = (struct Line *)malloc(sizeof(struct Line));
+    newln->valid = 1;
+    newln->tag = tag_bits;
+    add_line_to_head(target_set,newln);
+}
+
+// free cache, every set of cache, and every line in set
+void free_cache(struct Cache *cache){
+    struct Set *set_to_free = cache->headSet;
+    while (!set_to_free){
+        struct Line *ln_to_free = set_to_free->headLine;
+        while (!ln_to_free){
+            struct Line *temp_ln = ln_to_free->next;
+            free(ln_to_free);
+            ln_to_free = temp_ln;
+        }
+        struct Set *temp_set = set_to_free->next;
+        free(set_to_free);
+        set_to_free = temp_set;
+    }
+    free(cache);
+}
+
+int main(int argc, char** argv){
+    int opt;
+    char op;
+    unsigned addr;
+    int size;
+
+    // parse flag commands
+    while(-1 != (opt = getopt(argc, argv, "vhs:E:b:t:"))) {
+        switch(opt) {
+            case 'v':
+                vFlag = 1;
+                break;
+            case 'h':
+                hFlag = 1;
+                break;
+            case 's':
+                s = atoi(optarg);
+                if(s < 0 && s > m ){
+                    printf("Error: s value must be in [0, word_length].\n");
+                    exit(-1);
+                }
+                break;
+            case 'E':
+                E = atoi(optarg);
+                if(E <= 0){
+                    printf("Error: E value must be larger than 0.\n");
+                    exit(-1);
+                }
+                break;
+            case 'b':
+                b = atoi(optarg);
+                if(b < 0 || b > m){
+                    printf("Error: b value must be in [0, word_length].\n");
+                    exit(-1);
+                }
+                break;
+            case 't':
+                trace = optarg;
+                break;
+            default:
+                break;
         }
     }
 
-    used[1] = mostFreq;
-    used[0] = leastFreq;
-    if (empty) {
-        return j;
-    }
+    t = m - s - b;
+    struct Cache *mycache = (struct Cache *)malloc(sizeof(struct Cache));
+    init_cache(mycache, s, E);
 
-    for (i = 0; i < lineNum; i++) {
-        if (!tempSet.lines[i].valid) {
-            return i;
-        }
+    // read trace file and access cache
+    FILE *traceFile;
+    traceFile = fopen(trace, "r");
+    if (!traceFile){
+        fprintf(stderr, "Error: Trace file cannot be opened.\n");
+        return -1;
     }
+    // scan the trace file for the flag
+    while(fscanf(traceFile, "%c %x, %d", &op, &addr, &size) > 0) {
+        if (vFlag) printf("%c %x, %d ", op, addr, size);
+        switch(op){
+            case 'L':
+                access_cache(mycache, addr);
+                break;
+            case 'S':
+                access_cache(mycache, addr);
+                break;
+            case 'M':
+                access_cache(mycache, addr);
+                access_cache(mycache, addr);
+                break;
+        }
+        if (vFlag) printf("\n");
+    }
+    fclose(traceFile);
+
+    free_cache(mycache);
+    printSummary(hitCount, missCount, evictCount);
+
     return 0;
-} // indexRet
-
-
-struct cacheResult runCache(struct Cache cache, struct cacheResult result, address addr, int lineNum, int temp, int b) {
-    int i, j;
-    int tagWidth = 64-temp;
-    int missCheck = result.h;
-    bool spaceCheck = TRUE;
-    address tag = addr >> temp;
-    int index = ((addr << tagWidth) >> (tagWidth + b));
-    struct Set tempSet = cache.sets[index];
-
-    for (i = 0; i < lineNum; i++) {
-        struct Line tempLine = tempSet.lines[i];
-        if (tempLine.valid) {
-            if (tag == tempLine.tag) {
-                tempLine.used += 1;
-                tempSet.lines[i] = tempLine;
-                result.h++;
-            }
-        }
-        else if (!tempLine.valid && spaceCheck) {
-            spaceCheck = FALSE;
-        }
-    }
-
-    if (result.h == missCheck) {
-        result.m++;
-    }
-    else {
-        return result;
-    }
-    int* used = (int*)malloc(sizeof(int)*2);
-    j = indexRet(used, lineNum, tempSet, spaceCheck);
-    tempSet.lines[j].used = used[1] + 1;
-    tempSet.lines[j].tag = tag;
-
-    if (spaceCheck) {
-        result.e++;
-    }
-    else {
-        printf("Space Check\n");
-        tempSet.lines[j].valid = TRUE;
-    }
-    return result;
-} // cacheResult
+}
