c82c333cbb6a295f8e1b6ab9104d29d5130568ac
PJ <olenderpj1@gmail.com>
2021-12-07 17:43:24 -0700

    third attempt

--- csim.c
+++ csim.c
@@ -1,236 +1,226 @@
+/*
+
+   OVERVIEW:
+   This code is an a simulator for how a cache would handle hits, misses, and evicitons.
+   The main cache is stored as an array of structs. Each struct represents an entry, and
+   a struct is made for every entry in each set. After all init work, fscanf reads
+   the infiles and makes calls to missOrHit, the cache simulation code.
+   hitOrMiss looks through all of the cache entries for a given set to see if a hit can
+   be made. If there is a hit, the timeStamp(my LRU implementation) and hit_count are
+   increased. If not hit is made, the findEviction function is called. This function
+   looks through all of the items in a given set. If there is an entry that is still
+   cold, it is used. If no cache is cold, the entry that hasnt been used in the longest
+   is found. The eviction total is updated and the evictee's index is returned. hitOrMiss
+   then overrides any needed values, updates timeStamps, updates misscounts, and returns.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <math.h>
 #include "cachelab.h"
+// globals////////////////////////////////////////////////////////////////////////////
+int lines_per_set;
+int timeStamp;//global timer that functions as LRU
+int hit_total= 0, miss_total= 0, eviction_total= 0;
 
-const int m = sizeof(long) * 8;
-int hitCount = 0, missCount = 0, evictCount = 0;
-int vFlag = 0, hFlag = 0;
-int s, b, E, t;
-char *trace;
-
-
-struct Line
+//the cPart struct represents an entry/line
+//The tagInfo is the entry tage and the cTime is the LRU
+struct cPart
 {
-    int valid ;
-    unsigned tag ;
-    struct Line *next;
+    int cTime;
+    long long tagInfo;
 };
 
-struct Set
+void printUsage()
 {
-    int E;
-    struct Line *headLine;
-    struct Set *next;
-};
-
-struct Cache
+    printf( "Usage: csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n" );
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+//this function is given a copy of the struct arr and the current set number, and uses that//
+//to calculate the best place to insert a new value. If the timestamp is 0, we can assume a//
+//cold cache and use that place. Otherwise, the loop searches with the entry with the //
+//smallest timestamp, as this entry would be the oldest and the best one to evict. //
+//Then, the eviction total is increased. //
+//The index of the entry that will be evicted/cold cache entry is returned //
+/////////////////////////////////////////////////////////////////////////////////////////////
+int findEviction(int setNum2, struct cPart res2[])
 {
-    int S;
-    struct Set *headSet;
-};
-
-// return the number of valid lines in a set
-int set_size(struct Set *set)
-{
-    struct Line *curr = set->headLine;
-    int size = 0;
-    while (curr != NULL){
-        ++size;
-        curr = curr->next;
-    }
-    return size;
-}
-
-// initialize set with capacity E and no valid lines
-void init_set(struct Set *set, int E){
-    set->E = E;
-    set->headLine = NULL;
-    set->next = NULL;
-}
-
-// initialize cache with 2^s empty sets
-void init_cache(struct Cache *cache, int s, int E) {
-    cache->S = (1 << s);
-    struct Set *thisset = (struct Set*)malloc(sizeof(struct Set));
-    init_set(thisset, E);
-    cache->headSet= thisset;
-    int i = 1;
-    for (i ; i < cache->S; ++i){
-        struct Set *nextset = (struct Set*)malloc(sizeof(struct Set));
-        init_set(nextset, E);
-        thisset->next = nextset;
-        thisset = nextset;
-    }
-}
-
-// delete the last line of a set
-void delete_last_line(struct Set *set){
-    struct Line *curr = set->headLine;
-    struct Line *prev = NULL;
-    struct Line *prev2 = NULL;
-    while (curr != NULL){
-        prev2 = prev;
-        prev = curr;
-        curr = curr->next;
-    }
-    if (prev2 != NULL){
-        prev2->next = NULL;
-    } else {
-        set->headLine = NULL;
-    }
-    if (prev != NULL) free(prev);
-}
-
-// add a (valid) line to the head of a set, evict when exceeds capacity
-void add_line_to_head(struct Set *set, struct Line *line){
-    if (set_size(set) == set->E){
-        delete_last_line(set);
-        evictCount++;
-        if (vFlag) printf("eviction ");
-    }
-    line->next = set->headLine;
-    set->headLine = line;
-}
-
-// move the matched line to head of the set
-void move_line_to_head(struct Set *set, struct Line *line, struct Line *prev){
-    if (prev != NULL){
-        prev->next = line->next;
-        line->next = set->headLine;
-        set->headLine = line;
-    }
-}
-
-// the procedure of fetching an address in the cache
-void access_cache(struct Cache *cache, unsigned address){
-    int tag_bits = address >> (s+b);
-    int set_bits = (address << t) >> (t+b);
-
-    // move to target_set according to set_bits
-    struct Set *target_set = cache->headSet;
-    int i;
-    for (i = 0; i < set_bits; ++i){
-        target_set = target_set->next;
-    }
-
-    struct Line *ln = target_set->headLine;
-    struct Line *prev = NULL;
-    while (ln != NULL){
-        // if there is a  hit
-        if (ln->valid && (ln->tag == tag_bits)){
-            hitCount++;
-            if (vFlag) printf("hit ");
-            move_line_to_head(target_set, ln, prev);
-            return ;
+    int index=lines_per_set*setNum2;
+    int x;
+    for (x=0; x<lines_per_set;x++)
+    {
+        if(res2[x+(lines_per_set*setNum2)].cTime==0 )
+        {
+            return (x+(setNum2*lines_per_set)) ;}
+        else if(res2[x+(lines_per_set*setNum2)].cTime< res2[index].cTime)
+        {
+            index=x+(setNum2*lines_per_set);
         }
-        prev = ln;
-        ln = ln->next;
     }
-
-    // if there is no match then miss and add a new line
-    missCount++;
-    if (vFlag) printf("miss ");
-    struct Line *newln = (struct Line *)malloc(sizeof(struct Line));
-    newln->valid = 1;
-    newln->tag = tag_bits;
-    add_line_to_head(target_set,newln);
+    eviction_total++;
+    return index;
 }
+///////////////////////////////////////////////////////////////////////////////////////////////
+//This function takes the current instruct, the needed memory address, the tag/set/block //
+//bits, and a copy of the struct arr. The appropriate set mask is created, and that is used //
+//to the set number of the current memoryaddress. The tag bits are also calculated. The loop //
+//then goes through the cache, only searching through the items that are in the same set //
+//as the memoryaddress. *Remember* All of the sets include lines_per_set lines, so that needs//
+//to be multiplied to the set index. If there is a hit, the hit total is updated,the //
+//time stamp is updated and increased and the function ends. //
+//If no hit is made, the miss total is increased. Then, the find eviction function is //
+//called. Once the function returns an index, that information is changed, the timestamp //
+//is updated and increased, and then the function terminates. //
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-// free cache, every set of cache, and every line in set
-void free_cache(struct Cache *cache){
-    struct Set *set_to_free = cache->headSet;
-    while (!set_to_free){
-        struct Line *ln_to_free = set_to_free->headLine;
-        while (!ln_to_free){
-            struct Line *temp_ln = ln_to_free->next;
-            free(ln_to_free);
-            ln_to_free = temp_ln;
+
+void hitOrMiss(char instruction, long long memoryaddress, int tag_bits1, int set_bits, int block_bits2, struct cPart res[])
+{
+    long long setMask;
+    long long setNum;
+    long long tagStuff;
+    int x;
+    int y;
+    //masking and tag bits//
+    setMask=((long long)pow(2, set_bits))-1;
+    setNum=((memoryaddress)>>(block_bits2))&setMask;
+    tagStuff=(( long long) memoryaddress)>>(set_bits+block_bits2);
+
+    //main hit or miss detection loop//
+    for (x=0; x<lines_per_set; x++)
+    {
+        if(tagStuff==((( long long) res[x+(setNum*lines_per_set)].tagInfo)>>(set_bits+block_bits2)) && res[x+(setNum*lines_per_set)].cTime != 0 )
+        {
+            res[x+(setNum*lines_per_set)].cTime=timeStamp;
+            timeStamp++;
+            hit_total++;
+            return;
         }
-        struct Set *temp_set = set_to_free->next;
-        free(set_to_free);
-        set_to_free = temp_set;
     }
-    free(cache);
+
+    //miss handling//
+    miss_total++;
+    y=findEviction(setNum, res);
+    res[y].tagInfo=memoryaddress;
+    res[y].cTime=timeStamp;
+    timeStamp++;
+
+    return;
 }
 
-int main(int argc, char** argv){
-    int opt;
-    char op;
-    unsigned addr;
-    int size;
+//////////////////////////////////
+//This code was already included//
+//I initialized some of my //
+//variables here, but otherwise //
+//unchanged. //
+//////////////////////////////////
+int main( int argc, char * argv[] )
+{
+    // using getopt(), parse the command line arguments
+    char * filename= NULL;
+    int option;
+    char verbose= 0;
+    lines_per_set= 0;
+    int s_bits= 0, block_bits= 0, tag_bits= 0;
+    char iRead=' ';
+    timeStamp=1;
+    FILE* traceIO;
 
-    // parse flag commands
-    while(-1 != (opt = getopt(argc, argv, "vhs:E:b:t:"))) {
-        switch(opt) {
-            case 'v':
-                vFlag = 1;
-                break;
+
+    while ( (option= getopt( argc, argv, "hvs:E:b:t:" )) != -1 )
+    {
+        switch( option )
+        {
             case 'h':
-                hFlag = 1;
+                printUsage();
+                exit(EXIT_SUCCESS);
+                break;
+            case 'v':
+                verbose= 1;
                 break;
             case 's':
-                s = atoi(optarg);
-                if(s < 0 && s > m ){
-                    printf("Error: s value must be in [0, word_length].\n");
-                    exit(-1);
-                }
+                s_bits= atoi( optarg );
                 break;
             case 'E':
-                E = atoi(optarg);
-                if(E <= 0){
-                    printf("Error: E value must be larger than 0.\n");
-                    exit(-1);
-                }
+                lines_per_set= atoi( optarg );
                 break;
             case 'b':
-                b = atoi(optarg);
-                if(b < 0 || b > m){
-                    printf("Error: b value must be in [0, word_length].\n");
-                    exit(-1);
-                }
+                block_bits= atoi( optarg );
                 break;
             case 't':
-                trace = optarg;
+                filename= optarg;
                 break;
             default:
-                break;
+                printf( "Unknown option encountered\n" );
+                printUsage();
+                exit(EXIT_FAILURE);
         }
     }
-
-    t = m - s - b;
-    struct Cache *mycache = (struct Cache *)malloc(sizeof(struct Cache));
-    init_cache(mycache, s, E);
-
-    // read trace file and access cache
-    FILE *traceFile;
-    traceFile = fopen(trace, "r");
-    if (!traceFile){
-        fprintf(stderr, "Error: Trace file cannot be opened.\n");
-        return -1;
+    if ( s_bits == 0 || lines_per_set == 0 || block_bits == 0 )
+    {
+        printf( "Value for cache parameter not passed on command line\n" );
+        printUsage();
+        exit(EXIT_FAILURE);
     }
-    // scan the trace file for the flag
-    while(fscanf(traceFile, "%c %x, %d", &op, &addr, &size) > 0) {
-        if (vFlag) printf("%c %x, %d ", op, addr, size);
-        switch(op){
-            case 'L':
-                access_cache(mycache, addr);
-                break;
-            case 'S':
-                access_cache(mycache, addr);
-                break;
-            case 'M':
-                access_cache(mycache, addr);
-                access_cache(mycache, addr);
-                break;
+
+//////////////////////////////////////////////////////////////////////////////////
+//The code that I added here is minimal. The cache is stored as an array of //
+//structs. These structs are replicas of entries. The tag bits are caculcated //
+//and the file is opened and read. The timestamp is initialize, and the input //
+//values for scanf are created. Then all of the values for the cache are changed//
+//to zero as an initialization. Next, the scanf is called on the trace file. //
+//This information is stored and then the appropriate calls are made. If an //
+//instruction is made, nothing happens, if a modify is made, a call to hitOrMiss//
+//(our cache simulator) is made, and then the hit total is increased to //
+//represent a data store. Otherwise, a call is made to the function. //
+//*Note* The hit/miss/eviction totals were made global to prevent them from //
+//being passed to hitOrMiss. Once the loop is complete, printSummary is called. //
+//*Note* Since the cache is stored as an array, all accesses have to consider //
+//the number of lines per set. As a result, all accesses here and in hitOrMiss //
+//multiply the set number by the number of lines in a set. //
+//////////////////////////////////////////////////////////////////////////////////
+
+    int totalParts=(int)(pow(2, s_bits)*lines_per_set);//caculates number of entries
+    struct cPart fullCache[totalParts];//cache Array
+    tag_bits=sizeof(long long)*8- (s_bits+block_bits);
+    int fd = open(filename, O_RDONLY);
+    read(fd, &iRead, 1);
+    timeStamp=1;
+    long long memAd=0;
+    char instruct='a';
+    int acSize= 0;
+    int x ;
+
+
+    //init//
+    for (x=0; x< totalParts; x++)
+    {
+        fullCache[x].cTime=0;
+        fullCache[x].tagInfo=0;
+    }
+
+
+    traceIO=fopen(filename, "r");
+    while(fscanf(traceIO, " %c %llx,%d", &instruct, &memAd, &acSize) ==3)
+    {
+        if (instruct=='I')
+        {
+            continue;
         }
-        if (vFlag) printf("\n");
+        else if(instruct=='M')
+        {
+            hitOrMiss(instruct, memAd, tag_bits, s_bits,block_bits, fullCache);
+            hit_total++;
+        }
+        else
+        {hitOrMiss(instruct, memAd, tag_bits, s_bits,block_bits, fullCache);}
     }
-    fclose(traceFile);
 
-    free_cache(mycache);
-    printSummary(hitCount, missCount, evictCount);
-
+    printSummary( hit_total, miss_total, eviction_total );
     return 0;
 }
